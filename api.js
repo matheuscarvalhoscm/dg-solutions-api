15e9a7bc914691effaab464ea69a52c6865bab89
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-07 15:03:41 -0300

    feat: implement `body-parser` usage

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./src/routes/router');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3003;
 
+app.use(bodyParser.json());
 app.use('/', router);
 
 app.listen(PORT, () => console.log(`ouvindo na porta: ${PORT}`));
