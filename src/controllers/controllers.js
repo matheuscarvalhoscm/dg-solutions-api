b583074348e78349453305a4234f2e1ae116e30f
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-07 15:03:00 -0300

    feat: add controller for new register

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -5,6 +5,14 @@ const getAllRegisters = async (_req, res) => {
   return res.status(200).json(registers);
 };
 
+const addRegister = async (req, res) => {
+  const { name, birthDate } = req.body;
+  const newRegister = await Register.addRegister(name, birthDate);
+
+  return res.status(201).json(newRegister);
+};
+
 module.exports = {
   getAllRegisters,
+  addRegister,
 }
