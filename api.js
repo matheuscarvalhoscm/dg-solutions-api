696b7ba3e2e9cd1ea8e8c6c6eacd47396bcc3131
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-07 17:02:43 -0300

    feat: add `cors` for api usage

--- api.js
+++ api.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = require('./src/routes/router');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3003;
 
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST']
+}));
 app.use(bodyParser.json());
 app.use('/', router);
 
