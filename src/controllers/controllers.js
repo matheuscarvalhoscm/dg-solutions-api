c92a5a81049889b9059afe9f24f6ef2565c7e099
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-08 16:12:44 -0300

    refactor: use request dictionary in all controllers

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,8 +1,9 @@
 const Register = require('../services/services');
+const { OK, CREATED, BAD_REQUEST } = require('../utils/dictionary');
 
 const getAllRegisters = async (_req, res) => {
   const registers = await Register.getAllRegisters();
-  return res.status(200).json(registers);
+  return res.status(OK).json(registers);
 };
 
 const addRegister = async (req, res) => {
@@ -10,9 +11,9 @@ const addRegister = async (req, res) => {
     const { name, birthDate } = req.body;
     const newRegister = await Register.addRegister(name, birthDate);
   
-    return res.status(201).json(newRegister);
+    return res.status(CREATED).json(newRegister);
   } catch (error) {
-    return res.status(400).json(error.message);
+    return res.status(BAD_REQUEST).json(error.message);
   }
 };
 
