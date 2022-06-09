49a91eaf2273386e09c157fde62c275ca5d37e22
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-09 17:28:12 -0300

    chore: change port const to use env variable

--- api.js
+++ api.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = require('./src/routes/router');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 app.use(cors({
   origin: 'http://localhost:8080',
